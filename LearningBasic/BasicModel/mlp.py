59dea92a7e4a53f61ae59250aabf0ac69df4eb93
awp4211 <1097028825@qq.com>
2016-09-17 09:28:59 +0800

    MultiLayer Perception

--- LearningBasic/BasicModel/mlp.py
+++ LearningBasic/BasicModel/mlp.py
@@ -32,7 +32,7 @@ def multilayer_perceptron(x,weights,biases):
     layer1 = tf.add(tf.matmul(x,weights['h1']),biases['b1'])
     layer1 = tf.nn.relu(layer1)
     # Hidden layer with RELU activation
-    layer2 = tf.add(tf.matmul(x,weights['h2']),biases['b2'])
+    layer2 = tf.add(tf.matmul(layer1,weights['h2']),biases['b2'])
     layer2 = tf.nn.relu(layer2)
     # Output layer with linear activiation
     out_layer = tf.matmul(layer2,weights['out'])+biases['out']
@@ -64,3 +64,25 @@ with tf.Session() as sess:
     sess.run(init)
     
     # Training Cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(mnist.train.num_examples/batch_size)
+        for i in range(total_batch):
+            batch_x,batch_y = mnist.train.next_batch(batch_size)
+            _,c = sess.run([optimizer,cost],feed_dict={x:batch_x,
+                                                       y:batch_y})
+            avg_cost += c/total_batch
+        if epoch % display_step == 0:
+             print("Epoch:",'%04d'%(epoch+1),"cost=","{:.9f}".format(avg_cost))
+    print("Optimization Done")
+    
+    # Test model
+    correct_prediction = tf.equal(tf.arg_max(pred,1),tf.arg_max(y,1))
+
+    # print(type(correct_prediction)) <class 'tensorflow.python.framework.ops.Tensor'>
+
+    # Calculate accuracy 
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
+    
+    # print(type(accuracy)) <class 'tensorflow.python.framework.ops.Tensor'>
+    print("Accuracy:",accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
