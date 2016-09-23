d5dc8466f9d5fdb5dad022a8b442fc7f208b349d
awp4211 <1097028825@qq.com>
2016-09-23 17:52:31 +0800

    word2vec_skip_gram

--- Udacity/word2vec.py
+++ Udacity/word2vec.py
@@ -11,6 +11,9 @@ import random
 import math
 import collections
 
+from matplotlib import pylab
+from sklearn.manifold import TSNE
+
 # global variable
 data_index = 0 # 采样时从文本的第一个词开始采样
 
@@ -59,7 +62,7 @@ def generate_batch(data,batch_size,num_skips=2,skip_window=1):
     assert batch_size % num_skips == 0
     assert num_skips <= 2 * skip_window
     batch = np.ndarray(shape=(batch_size),dtype=np.int32)
-    labels = np.ndarray(shape(batch_size,1),dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size,1),dtype=np.int32)
     span = 2 * skip_window + 1
     buffer = collections.deque(maxlen=span)
     # 向队列buffer中添加span个词的索引
@@ -91,11 +94,12 @@ def train(filename="text8.zip",
           num_skips=2,# 每个词采样时作为中心词使用的次数
           valid_size=16,# 随机选取的词的个数去评估模型
           valid_window=100,# 从当前词的前多少个选取测试样本
-          num_sampled=64
+          num_sampled=64,# 负采样数
+          num_steps=100001
           ):
     print('...... Reading data from zip file......')
     words = read_data(filename)
-    print('Data Size = '%len(words))
+    print('Data Size = {0}'.format(len(words)))
     
     print('...... Transfer word data to word index,dictionary ......')
     data, count, dictionary, reverse_dictionary = build_dataset(vocabulary_size,words)
@@ -106,7 +110,7 @@ def train(filename="text8.zip",
     # 即随机选取k个sequence中的元素
     valid_examples = np.array(random.sample(range(valid_window),valid_size))
     graph = tf.Graph()
-    with graph.as_default():
+    with graph.as_default(),tf.device('/cpu:0'):
         # Input data
         train_dataset = tf.placeholder(tf.int32,shape=[batch_size])
         train_labels = tf.placeholder(tf.int32,shape=[batch_size,1])
@@ -138,4 +142,80 @@ def train(filename="text8.zip",
         
         # Optimizer
         optimizer = tf.train.AdadeltaOptimizer(1.0).minimize(loss)
+        # Compute the similarity between minibatch examples and all embeddings.
+        # We use the cosine distance:
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+        normalized_embeddings = embeddings / norm
+        valid_embeddings = tf.nn.embedding_lookup(
+            normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
     
+    print('...... Training ......')    
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        average_loss = 0
+        for step in range(num_steps):
+            batch_data, batch_labels = generate_batch(data,
+                                                      batch_size, 
+                                                      num_skips, 
+                                                      skip_window
+                                                      )
+            feed_dict = {train_dataset: batch_data, train_labels: batch_labels}
+            _, l = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += l
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss /= 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step %d: %f' % (step, average_loss))
+                average_loss = 0
+            # note that this is expensive (~20% slowdown if computed every 500 steps)
+            if step % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8  # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = reverse_dictionary[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+        final_embeddings = normalized_embeddings.eval()
+        save_obj('text8_embed.pickle', final_embeddings)
+    print('...... Training finished ......')
+    return final_embeddings,reverse_dictionary
+    
+def save_obj(pickle_file,obj):
+    import cPickle as pickle
+    import os
+    try:
+        f = open(pickle_file, 'wb')
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+        f.close()
+    except Exception as e:
+        print('Unable to save data to', pickle_file, ':', e)
+        raise
+    statinfo = os.stat(pickle_file)
+    print('Compressed pickle size:{0}MB'.format(statinfo.st_size/(1024.*1024.)))
+    
+def tsne_and_plot(num_points,
+                  final_embeddings,
+                  labels):
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points + 1, :])
+    assert two_d_embeddings.shape[0] >= len(labels), 'More labels than embeddings'
+    pylab.figure(figsize=(15, 15))  # in inches
+    for i, label in enumerate(labels):
+        x, y = two_d_embeddings[i, :]
+        pylab.scatter(x, y)
+        pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
+                       ha='right', va='bottom')
+    pylab.show()
+
+if __name__ == "__main__":
+    num_points = 400
+    final_embeddings,reverse_dictionary = train()
+
+    words = [reverse_dictionary[i] for i in range(1, num_points + 1)]
+    tsne_and_plot(num_points,final_embeddings,words)
