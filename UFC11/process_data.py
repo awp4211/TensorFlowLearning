e71d984814121c76f408f935844fc5287266a292
awp4211 <1097028825@qq.com>
2016-10-12 17:34:04 +0800

    process data

--- UFC11/process_data.py
+++ UFC11/process_data.py
@@ -5,6 +5,8 @@ from PIL import Image
 
 import numpy as np
 
+clazz_count = 11
+
 def process_data(width=256,height=256):
     
     _train_path_file = 'UFC11/_train_file.txt'
@@ -16,29 +18,69 @@ def process_data(width=256,height=256):
     train_label = open(_train_label_file,'rt')
     test_file = open(_test_path_file,'rt')
     test_label = open(_test_label_file,'rt')
+
+    train_set_x = []
+    train_set_y = []
+    test_set_x = []
+    test_set_y = []     
     
-    #===training set 33528 = 24 * 1397
-    for img_path in train_file.readlines():
-        pass
-    
-    #===training label 1397
-    for label in train_label.readlines():
-        pass        
-     
+    index = 0
     #===testing set 4872 = 24 * 203
     for img_path in test_file.readlines():
-        pass
+        img_path = img_path[0:-2]#去掉/r/n
+        vec = convert_image_to_vec(img_path,width,height)
+        test_set_x.append(vec)
+        index += 1
+        if index%100 == 0:
+            print('processed {0} images'.format(index))
+
+    test_set_x = np.asarray(test_set_x)
     
     #===testing set 203
     for label in test_label.readlines():
-        pass
+        vec = one_hot_encoder(label,clazz_count)
+        test_set_y.append(vec)    
     
+    test_set_y = np.asarray(test_set_y)
+    
+    index = 0
+    #===training set 33528 = 24 * 1397
+    for img_path in train_file.readlines():
+        img_path = img_path[0:-2]#去掉/r/n
+        vec = convert_image_to_vec(img_path,width,height)
+        train_set_x.append(vec)
+        index += 1
+        if index%100 == 0:
+            print('processed {0} images'.format(index))
+            
+    train_set_x = np.asarray(train_set_x)
+    
+    #===training label 1397
+    for label in train_label.readlines():
+        vec = one_hot_encoder(label,clazz_count)
+        train_set_y.append(vec)
+    
+    train_set_y = np.asarray(train_set_y)
+    
+    print(train_set_x.shape)
+    print(train_set_y.shape)
+    print(test_set_x.shape)
+    print(test_set_y.shape)
+    
+  
+def one_hot_encoder(label,class_count):
+    mat = np.asarray(np.zeros(class_count),dtype='float64').reshape(1,class_count)
+    for i in range(class_count):
+        if i == label:
+            mat[0][i] = 1
+    return mat.flatten()
+        
 
 def convert_image_to_vec(img,width,height):
     im = Image.open(img)
     im = im.convert('L')
     mat = np.asarray(np.arange(width*height),dtype='float32').reshape(width,height)
-    im = im.resize((width,height),Image.ANTIALIAS)  
+    im = im.resize((width,height),Image.ANTIALIAS)
     for i in range(0,list(im.size)[0]):
         for j in range(0,list(im.size)[1]):
             mat[i][j] = float(im.getpixel((i,j)))/255.0
@@ -49,4 +91,4 @@ def load_data_set():
     pass
         
 if __name__ == '__main__':
-    process_data()
+    process_data(width=128,height=128)
