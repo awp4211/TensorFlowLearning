69c8e0981dd9b5763a2dbb244c69b03f657b627c
awp4211 <1097028825@qq.com>
2016-10-14 08:18:45 +0800

    res lstm

--- UFC11/ufc11_res_lstm.py
+++ UFC11/ufc11_res_lstm.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 
 """
+Author:XiyouZhaoC
 Residual network and LSTM
 this code running on tensorflow 0.10.0 platform
 By using residual network(ResNet38)(CNN) to extract features and
     using LSTM(single layer LSTM) to process sequence data
 
-
-    
-UFC11 dataset
+UFC11 dataset contains 1600 videos and hava been classified 11 classes 
 """
 import tensorflow as tf
 import process_data as pd
 from collections import namedtuple
+from math import sqrt
+
+# Dataset count
+n_train_example = 33528
+n_test_example = 4872
 
 # Network Parameter
 learning_rate = 0.1
 pic_batch_size = 240
 fps = 24
 video_batch_size = pic_batch_size / fps
+n_classes = 11
+
+
+# LSTM Parameter
+n_hidden_units = 128
 
 
 """
@@ -56,7 +65,6 @@ def conv2d(x,n_filters,
         return conv   
 
 def residual_network(x,
-                     n_output,
                      activation=tf.nn.relu):
     LayerBlock = namedtuple(
                 'LayerBlock',['num_repeats','num_filters','bottleneck_size'])
@@ -108,7 +116,7 @@ def residual_network(x,
                           activation=activation,
                           name=name+'/conv_bottlneck')
             print('{0}/conv_bottleneck,shape = {1}'.format(name,conv.get_shape()))
-            #3*3卷积[batch,7,7,bottlneck_size]
+            #3*3卷积[batch,7,7,bottlneck_size](stride=1,padding=SAME卷积之后维度不变)
             
             conv = conv2d(conv,block.num_filters,k_h=1,k_w=1,
                           padding='VALID',stride_h=1,stride_w=1,
@@ -145,12 +153,115 @@ def residual_network(x,
          net.get_shape().as_list()[3]])
     print('After residual network shape = {0}'.format(net.get_shape()))
     
-        
-def train():
+    return net
+
+
     
+def lstm_layer(x):
+    # x :[pic_batch_size,1024]
+    # transpose to [video_batch_size,fps,1024]
+    # get input     
+    n_inputs = x.get_shape().as_list()[-1]
     
+    # Define weights
+    weights = {
+        #(n_inpus=1024,n_hidden_units=128)
+        'in':tf.Variable(tf.random_normal([n_inputs,n_hidden_units])),
+        #(128,10)
+        'out':tf.Variable(tf.random_normal([n_hidden_units,n_classes]))
+    }
+    biases = {
+        #(n_hidden_units=128,)
+        'in':tf.Variable(tf.constant(0.1,shape=[n_hidden_units,])),
+        #(n_classes=11,)
+        'out':tf.Variable(tf.constant(0.1,shape=[n_classes,]))
+    }
+    
+    # x[pic_batch,n_inputs] ==> [video_batch_size,fps,n_inputs]
+    x = tf.reshape(x,[-1,fps,n_inputs])
+    # x[video_batch_size,fps,n_inputs] ==> [video_batch_size * fps,n_inputs]
+    x = tf.reshape(x,[-1,n_inputs])
+    # x_in ==> (video_batch_size * fps,n_hidden_units)
+    x_in = tf.matmul(x,weights['in']) + biases['in']
+    # x_in ==> (video_batch_size,fps,n_hidden_units)
+    x_in = tf.reshape(x_in,[-1,fps,n_hidden_units])
+    
+    # cell
+    # forget_bias = 1.0 represents all information can through lstm
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units,
+                                            forget_bias=1.0,
+                                            state_is_tuple=True)
+    _init_state = lstm_cell.zero_state(video_batch_size,dtype=tf.float32)
+    outputs,states = tf.nn.dynamic_rnn(lstm_cell,
+                                       x_in,
+                                       initial_state=_init_state,
+                                       time_major=False
+                                       )
+    # unpack to list[(video_batch_size,outpits)*fps]
+    # transpose:[video_batch_size,fps,n_hidden_units]
+    #               ==> [fps,video_batch_size,n_hidden_units]
+    outputs = tf.unpack(tf.transpose(outputs,[1,0,2]))
+    results = tf.matmul(outputs[-1],weights['out']) + biases['out']
+    return results
+    
+def train_res_lstm(width=256,height=256):
     print('...... loading the dataset ......')
     train_set_x,train_set_y,test_set_x,test_set_y = pd.load_data_set()
     
     print('...... building the model ......')
-    x = tf.placeholder(tf)
+    x = tf.placeholder(tf.float32,[None,width*height])
+    y = tf.placeholder(tf.float32,[None,10])
+    y_res = residual_network(x)
+    y_pred = lstm_layer(y_res)
+    
+    # Define loss and training functions
+    cross_entropy = -tf.reduce_sum(y * tf.log(y_pred))
+    optimizer = tf.train.AdamOptimizer().minimize(cross_entropy)
+    
+    # Monitor Accuracy
+    correct_prediction = tf.equal(tf.argmax(y_pred,1),tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,'float'))
+
+    best_acc = 0.
+    
+    # Session
+    with tf.Session() as sess:
+        print('...... initializating varibale ...... ')
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        
+        n_epochs = 5
+        print('...... start to training ......')
+        for epoch_i in range(n_epochs):
+            # Training 
+            train_accuracy = 0
+            for batch_i in range(n_train_example//pic_batch_size):
+                batch_xs = train_set_x[batch_i*pic_batch_size:(batch_i+1)*pic_batch_size]
+                batch_ys = train_set_y[batch_i*video_batch_size:(batch_i+1)*video_batch_size]
+                train_accuracy += sess.run([optimizer,accuracy],
+                                           feed_dict={
+                                                x:batch_xs,
+                                                y:batch_ys}
+                                                )[1]
+            train_accuracy /= (n_test_example//pic_batch_size)
+            
+            # Validation
+            valid_accuracy = 0.
+            for batch_i in range(n_test_example//pic_batch_size):
+                batch_xs = test_set_x[batch_i*pic_batch_size:(batch_i+1)*pic_batch_size]
+                batch_ys = test_set_y[batch_i*video_batch_size:(batch_i+1)*video_batch_size]
+                valid_accuracy += sess.run(accuracy,
+                                           feed_dict={
+                                                x:batch_xs,
+                                                y:batch_ys})
+            valid_accuracy /= (n_test_example//pic_batch_size)
+            print('epoch:{0},train_accuracy:{1},valid_accuracy:{2}'.format(epoch_i,train_accuracy,valid_accuracy))
+            if(train_accuracy > best_acc):
+                best_acc > train_accuracy
+    
+    print('...... training finished ......')
+    print('...... best accuracy{0} ......'.format(best_acc))
+
+
+if __name__ == '__main__':
+    train_res_lstm()
