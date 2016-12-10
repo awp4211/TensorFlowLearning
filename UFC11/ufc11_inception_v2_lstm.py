f4c81345186753b3a815b47d833b8ee806cb1235
awp4211 <1097028825@qq.com>
2016-12-10 17:41:54 +0800

    InceptionV2_SLSTM

--- UFC11/ufc11_inception_v2_lstm.py
+++ UFC11/ufc11_inception_v2_lstm.py
@@ -616,28 +616,185 @@ def inception_v2(x,
     # 96*96*1   ===> 3*3*1024
     #================================DEBUG=====================================
     print('After InceptionV2, shape = {0}'.format(net.get_shape()))
+    
+    with tf.name_scope('Average_pool'):
+        net = tf.nn.avg_pool(net,
+                             ksize=[1,net.get_shape().as_list()[1],
+                                    net.get_shape().as_list()[1],1],
+                             strides=[1,1,1,1],
+                             padding='VALID')
+    print('After avg pool size = {0}'.format(net.get_shape()))
+    
+    with tf.name_scope('Flatten_layer'):
+        net = tf.reshape(
+            net,
+            [-1,net.get_shape().as_list()[1] * 
+            net.get_shape().as_list()[2] * 
+            net.get_shape().as_list()[3]])
+            
+    # Dropout
+    with tf.name_scope('Dropout'):
+        net = tf.nn.dropout(net,keep_prob=dropout_keep_prob)
+    print('After InceptionV2, shape = {0}'.format(net.get_shape()))
     return net
 
-    
+def stacked_lstm_layer(x,
+                       n_lstm):
+    print('============================LSTM==================================')
+    # x :[pic_batch_size,1024]
+    # transpose to [video_batch_size,fps,1024]
+    # get input     
+    n_inputs = x.get_shape().as_list()[-1]
+    print('LSTM Layer n_inputs={0}'.format(n_inputs))    
 
-def test_net_shape(width,height):
+    # Define weights
+    weights = {
+        #(n_inpus=1024,n_hidden_units=128)
+        'in':tf.Variable(tf.random_normal([n_inputs,n_hidden_units])),
+        #(n_hidden_units=128,n_classes=11)
+        'out':tf.Variable(tf.random_normal([n_hidden_units,n_classes]))
+    }
+    biases = {
+        #(n_hidden_units=128,)
+        'in':tf.Variable(tf.constant(0.1,shape=[n_hidden_units,])),
+        #(n_classes=11,)
+        'out':tf.Variable(tf.constant(0.1,shape=[n_classes,]))
+    }
+    with tf.name_scope('LSMT_reshape'):
+        # x[pic_batch,n_inputs] ==> [video_batch_size,fps,n_inputs]
+        x = tf.reshape(x,[-1,fps,n_inputs])
+        # x[video_batch_size,fps,n_inputs] ==> [video_batch_size * fps,n_inputs]
+        x = tf.reshape(x,[-1,n_inputs])
+    print('LSTM_reshape,shape = {0}'.format(x.get_shape()))
+    
+    with tf.name_scope('LSTM_upscale'):
+        # x_in ==> (video_batch_size * fps,n_hidden_units)
+        x_in = tf.matmul(x,weights['in']) + biases['in']
+        # x_in ==> (video_batch_size,fps,n_hidden_units)
+        x_in = tf.reshape(x_in,[-1,fps,n_hidden_units])
+    print('LSTM_upscale,shape = {0}'.format(x_in.get_shape()))
+    
+    with tf.name_scope('LSTM_cell'):
+        # cell
+        # forget_bias = 1.0 represents all information can through lstm
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units,
+                                                forget_bias=1.0,
+                                                state_is_tuple=False)
+        # stacked lstm
+        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * n_lstm,
+                                                   state_is_tuple=False)
+        _init_state = stacked_lstm.zero_state(video_batch_size,dtype=tf.float32)
+        
+        outputs,states = tf.nn.dynamic_rnn(stacked_lstm,
+                                           x_in,
+                                           initial_state=_init_state,
+                                           time_major=False
+                                           )
+        #==================================DUBUG===================================
+                                           
+        #[10,24,128][video_batch_size,fps,n_hidden_units]
+        print('After LSTM layer dynamic run,output shape = {0}'.format(outputs.get_shape()))
+        outputs = tf.unpack(tf.transpose(outputs,[1,0,2]))
+    print('LSTM_cell,outputs\' shape = {0}'.format(len(outputs)))
+    
+    with tf.name_scope('LSTM_downscale'):
+        results = tf.matmul(outputs[-1],weights['out']) + biases['out']
+    print('LSTM_downscale,shape = {0}'.format(results.get_shape()))
+    
+    return results    
+
+def test_net(width,height):
     x = tf.placeholder(tf.float32,[None,width*height])
-    keep_prob = tf.placeholder(tf.float32) 
+    y = tf.placeholder(tf.float32,[None,n_classes])
+    y_inception = inception_v2(x,width,height,0.8)
+    y_pred = stacked_lstm_layer(y_inception,7)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_pred,y))
+    optimizer = tf.train.AdamOptimizer(0.0001).minimize(cost)
+    
+def train_inception_v2_stacked_lstm(width,
+                                    height,
+                                    n_lstm=7,
+                                    dropout_keep_prob=0.8,
+                                    learning_rate=0.00001,
+                                    n_epochs=200):
+    print('...... loading the dataset ......')
+    train_set_x,train_set_y,test_set_x,test_set_y = pd.load_data_set(width,height)
+    
+    x = tf.placeholder(tf.float32,[None,width*height]) # input
+    y = tf.placeholder(tf.float32,[None,n_classes])    # label
+    keep_prob = tf.placeholder(tf.float32)             # dropout_keep_prob
+    
     y_inception = inception_v2(x,width,height,keep_prob)
+    y_pred = stacked_lstm_layer(y_inception,n_lstm)
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_pred,y))
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+    
+    correct_prediction = tf.equal(tf.argmax(y_pred,1),tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+    best_acc = 0.
+    best_acc_epoch = 0
+    
+    init = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        print('...... initializating varibale ...... ')
+        sess.run(init)
+        
+        print('...... start to training ......')
+        for epoch_i in range(n_epochs):
+            # Training 
+            train_accuracy = 0.
+            for batch_i in range(n_train_example//pic_batch_size):
+                
+                batch_xs = train_set_x[batch_i*pic_batch_size:(batch_i+1)*pic_batch_size]
+                batch_ys = train_set_y[batch_i*video_batch_size:(batch_i+1)*video_batch_size]
+                _,loss,acc = sess.run([optimizer, cost, accuracy],
+                                           feed_dict={
+                                                x:batch_xs,
+                                                y:batch_ys,
+                                                keep_prob:dropout_keep_prob}
+                                                )
+                #print('epoch:{0},minibatch:{1},y_res:{2}'.format(epoch_i,batch_i,yy_res))
+                #print('epoch:{0},minibatch:{1},y_pred:{2}'.format(epoch_i,batch_i,yy_pred))
+                print('epoch:{0},minibatch:{1},cost:{2},train_accuracy:{3}'.format(epoch_i,batch_i,loss,acc))
+                train_accuracy += acc
+
+            train_accuracy /= (n_train_example//pic_batch_size)
+            print('----epoch:{0},training acc = {1}'.format(epoch_i,train_accuracy))
+            
+            # Validation
+            valid_accuracy = 0.
+            for batch_i in range(n_test_example//pic_batch_size):
+                batch_xs = test_set_x[batch_i*pic_batch_size:(batch_i+1)*pic_batch_size]
+                batch_ys = test_set_y[batch_i*video_batch_size:(batch_i+1)*video_batch_size]
+                valid_accuracy += sess.run(accuracy,
+                                           feed_dict={
+                                                x:batch_xs,
+                                                y:batch_ys,
+                                                keep_prob:1.0})
+            valid_accuracy /= (n_test_example//pic_batch_size)
+            print('epoch:{0},train_accuracy:{1},valid_accuracy:{2}'.format(epoch_i,train_accuracy,valid_accuracy))
+            if(train_accuracy > best_acc):
+                best_acc_epoch = epoch_i
+                best_acc = train_accuracy
+            print('---epoch:{0},current best accuracy = {1} in epoch_{2}'.format(epoch_i,best_acc,best_acc_epoch))
+    
+    print('...... training finished ......')
+    print('...... best accuracy{0} @ epoch_{1}......'.format(best_acc,best_acc_epoch))                        
+    
     
     
 if __name__ == '__main__':
+    
     if len(sys.argv) == 3:
-        # python *.py width height
         w = int(sys.argv[1])
         h = int(sys.argv[2])
-        print('...... training Inception v2 and LSTM :width={0},height={1}'.format(w,h))
-    elif len(sys.argv) == 4:
-        # python *.py width height test_net_shape
+        print('...... training inception v1 and lstm network:width = {0},height = {1}'.format(sys.argv[1],sys.argv[2]))
+        train_inception_v2_stacked_lstm(width=w,height=h)
+    
+    if len(sys.argv) == 4:
         w = int(sys.argv[1])
         h = int(sys.argv[2])
-        print('...... test Inception v2 and LSTM Net shape:width={0},height={1}'.format(w,h))
-        test_net_shape(w,h)
-    else:
-        #len(sys.argv) == 1 python *.py
-        pass
+        print('...... testing inception v1 and stacked lstm network:width = {0},height = {1}'.format(sys.argv[1],sys.argv[2]))
+        test_net(width=w,height=h)
