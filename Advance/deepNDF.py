71bcf0e5c9828a8ee03136f631f455c67eb80b55
awp4211 <1097028825@qq.com>
2017-03-06 22:26:50 +0800

    deep neural decision forest

---
+++ Advance/deepNDF.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This is a tutorial implemention of Deep Neural Decision Forests
+(dNDF).The paper was published in ICCV(the Open Access vision,provided by
+the Computer Vision Foundation).
+
+Deep Neural Decision Forest,ICCV 2015,proposed a great way to incorporate a 
+neural network with a decision forest.Durning the optimization,the terminal
+(leaf)node has to be updated after each epoch.
+"""
