6926d71d8325942e60c71f602d1dc60ac885aee4
awp4211 <1097028825@qq.com>
2017-03-14 10:50:41 +0800

    DCGAN

--- Advance/deep_convolutional_generative_adversarial_networks.py
+++ Advance/deep_convolutional_generative_adversarial_networks.py
@@ -10,11 +10,13 @@ convolutional generative adversarial networkss
 import math
 import tensorflow as tf
 import numpy as np
+import os
 
 from tensorflow.contrib import layers
 from tensorflow.contrib import losses
 from tensorflow.contrib.framework import arg_scope
-
+from scipy.misc import imsave
+from progressbar import ETA, Bar, Percentage, ProgressBar
 
 
 def encoder(input_tensor, output_size):
@@ -88,3 +90,118 @@ def decoder(input_tensor):
     return net
 
 
+def concat_elu(inputs):
+    return tf.nn.elu(tf.concat(3, [-inputs, inputs]))
+
+
+class Generator(object):
+
+    def update_params(self, inputs):
+        """
+        update parameters of the network
+        :param inputs:a batch of flattened images
+        :return:current loss value
+        """
+        return NotImplementedError()
+
+    def generate_and_save_images(self, num_samples, directory):
+        """
+        generates the images using the model and saves them in the directory
+        :param num_samples: number of samples to generate
+        :param directory:
+        :return:
+        """
+        imgs = self.sess.run(self.sampled_tensor)
+        for k in range(imgs.shape[0]):
+            imgs_folder = os.path.join(directory, 'dcgan_imgs')
+            if not os.path.exists(imgs_folder):
+                os.makedirs(imgs_folder)
+
+            imsave(os.path.join(imgs_folder, '%d.png') % k,
+                   imgs[k].reshape(28, 28))
+
+
+class GAN(Generator):
+
+    def __init__(self, hidden_size, batch_size, learning_rate):
+        self.input_tensor = tf.placeholder(tf.float32, [None, 28*28])
+
+        with arg_scope([layers.conv2d, layers.conv2d_transpose],
+                       activation_fn=concat_elu,
+                       normalizer_fn=layers.batch_norm,
+                       normalizer_params={'scale':True}):
+            with tf.variable_scope('model'):
+                D1 = discriminator(self.input_tensor) # postive examples
+                D_params_sum = len(tf.trainable_variables())
+                G = decoder(tf.random_normal([batch_size, hidden_size]))
+                self.sampled_tensor = G
+
+            with tf.variable_scope('model',reuse=True):
+                D2 = discriminator(G) # generated examples
+
+            D_loss = self.__get_discriminator_loss(D1, D2)
+            G_loss = self.__get_generator_loss(D2)
+
+            params = tf.trainable_variables()
+            D_params = params[:D_params_sum]
+            G_params = params[D_params_sum:]
+            global_step = tf.contrib.framework.get_or_create_global_step()
+            self.train_discrimator = layers.optimize_loss(
+                D_loss, global_step, learning_rate / 10, 'Adam', variables=D_params, update_ops=[])
+            self.train_generator = layers.optimize_loss(
+                G_loss, global_step, learning_rate, 'Adam', variables=G_params, update_ops=[])
+
+            self.sess = tf.Session()
+            self.sess.run(tf.global_variables_initializer())
+
+    def __get_discriminator_loss(self, D1, D2):
+        """
+        Loss for the discriminator network
+        :param D1:logits computed with a discriminator networks from real images
+        :param D2:logits computed with a discriminator networks from generated images
+        :return:Cross entropy loss, postive samples have implicit labels 1, negative 0ss
+        """
+        return (losses.sigmoid_cross_entropy(D1, tf.ones(tf.shape(D1))) +
+                losses.sigmoid_cross_entropy(D2, tf.zeros(tf.shape(D1))))
+
+    def __get_generator_loss(self, D2):
+        """
+        Loss for generator.Maximize probability of generating images that
+            discrimator cannot differentiate.
+        :param D2:
+        :return:
+        """
+        return losses.sigmoid_cross_entropy(D2, tf.ones(tf.shape(D2)))
+
+    def update_params(self, inputs):
+        d_loss_value = self.sess.run(self.train_discrimator,
+                                     {self.input_tensor,inputs})
+
+        g_loss_value = self.sess.fun(self.train_generator)
+
+        return g_loss_value
+
+
+if __name__ == '__main__':
+    from tensorflow.examples.tutorials.mnist import input_data
+
+    # parameter
+    hidden_size = 128
+    batch_size = 128
+    max_epoch = 100
+    learning_rate = 1e-2
+    updates_per_epoch = 1000
+    mnist = input_data.read_data_sets('MNIST/', one_hot=True)
+
+    model = GAN(hidden_size, batch_size, learning_rate)
+    for epoch in range(max_epoch):
+        training_loss = 0.0
+        pbar = ProgressBar()
+        for i in pbar(range(updates_per_epoch)):
+            images,_ = mnist.train.next_batch(batch_size)
+            loss_value = model.update_params(images)
+            training_loss += loss_value
+
+        training_loss = training_loss / (updates_per_epoch * batch_size)
+        print("Loss %f" % training_loss)
+        model.generate_and_save_images(batch_size,"GAN_IMG/")
